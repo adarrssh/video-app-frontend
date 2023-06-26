37d81d23d3e19edaea3d04366b7344d4e87da3a3
adarsh <adarsh@growpital.com>
2023-06-27 00:31:34 +0530

    testing the sockets

--- src/Player.js
+++ src/Player.js
@@ -5,14 +5,19 @@ const Player = () => {
   const { id:videoId } = useParams();
   const videoRef = useRef(null);
 
+  const socket = io('http://localhost:4000'); // Replace with your server URL
   useEffect(() => {
-    const socket = io('https://video-app-g2dr.onrender.com'); // Replace with your server URL
 
     // Socket event listeners
     socket.on('connect', () => {
       console.log('Connected to server');
     });
 
+    // socket.on('videoTimeChanged',(data)=>{
+    //   console.log(data);
+    //   videoRef.current.currentTime = data
+    // })
+    
     socket.on('disconnect', () => {
       console.log('Disconnected from server');
     });
@@ -29,11 +34,19 @@ const Player = () => {
     }
   };
 
+  const handleSeeked = () => {
+    console.log('User clicked on the video timeline '+videoRef.current.currentTime)
+    // socket.emit('videoTimeChanged',videoRef.current.currentTime)
+  };
+
+
     return (
         <div className="player"  style={{ width: '70%', height: '60%' }}>
             <header className="App-header">
-                <video controls muted autoPlay ref={videoRef} style={{ width: '70%', height: '60%' }}>
-                    <source src={`https://video-app-g2dr.onrender.com/video/${videoId}`} type="video/mp4"></source>
+                <video controls muted autoPlay ref={videoRef} 
+                onSeeked={handleSeeked}
+                 style={{ width: '70%', height: '60%' }}>
+                    <source src={`http://localhost:4000/video/${videoId}`} type="video/mp4"></source>
                 </video>
                 <button onClick={handleForward}>forward</button>
             </header>
