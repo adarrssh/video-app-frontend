dd2873c5bc30a040b5fcb637d4ec30c81e01e7f9
adarsh <adarsh@growpital.com>
2023-06-26 21:56:57 +0530

    fix: removed sockets again

--- src/Player.js
+++ src/Player.js
@@ -1,98 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams  } from 'react-router-dom';
-import io from 'socket.io-client';
-const socket = io('https://video-app-g2dr.onrender.com/');
-// const socket = io('http://localhost:4000/')
-const Player = (props) => {
-  const { id } = useParams();
+const Player = () => {
+  const { id:videoId } = useParams();
   const videoRef = useRef(null);
-  const [videoId, setVideoId] = useState(id);
-  const [isPaused, setIsPaused] = useState(false);
-
-    // useEffect(()=>{
-    //     socket.on('sync',()=>{
-    //     Forward()
-    //     })
-    // },[])
-    const handleForward = () => {
-      if (videoRef.current) {
-        videoRef.current.currentTime += 10; // Forward by 10 seconds
-        // socket.emit('sync',{time:10})
-      }
-    };
-    
-    // const Forward = () => {
-    //       videoRef.current.currentTime += 10; // Forward by 10 seconds
-    //   };
-
-  useEffect(() => {
-    // Listen for 'videoPaused' event
-    socket.on('videoPaused', () => {
-        console.log('incoming videoPaused event');
-    //   if (!videoRef.current.paused) {
-        videoRef.current.pause();
-        setIsPaused(true);
-    //   }
-    });
-
-    // Listen for 'videoPlayed' event
-    socket.on('videoPlayed', () => {
-    //   if (videoRef.current.paused && isPaused) {
-        console.log('video play event');
-        videoRef.current.play();
-        setIsPaused(false);
-    //   }
-    });
-
-    // Emit 'videoPaused' event when the video is paused
-    const handlePause = () => {
-        console.log('paused');
-      if (videoRef.current.paused) {
-        socket.emit('videoPaused');
-      }
-    };
-
-    // Emit 'videoPlayed' event when the video is played
-    const handlePlay = () => {
-    //   if (videoRef.current.paused && isPaused) {
-        console.log('video played');
-        socket.emit('videoPlayed');
-    //   }
-    };
-
-    // listend for forward event 
-    socket.on('forward video',()=>{
-        console.log('forward event');
-        // if (videoRef.current) {
-            videoRef.current.currentTime += 10;
-        //   }
-    })
-    // Attach event listeners to the video element
-    if (videoRef.current) {
-      videoRef.current.addEventListener('pause', handlePause);
-      videoRef.current.addEventListener('play', handlePlay);
-    }
-
-    return () => {
-      // Clean up event listeners and socket listeners when the component is unmounted
-      if (videoRef.current) {
-        videoRef.current.removeEventListener('pause', handlePause);
-        videoRef.current.removeEventListener('play', handlePlay);
-      }
-      socket.off('videoPaused');
-      socket.off('videoPlayed');
-    };
-  }, [isPaused]);
-
-  const handleForward = () => {
-    socket.emit('forward video')
-    console.log('forward video');
-    if (videoRef.current) {
-      videoRef.current.currentTime += 10; // Forward by 10 seconds
-    }
-  };
-
-  
 
 
     return (
@@ -101,8 +11,6 @@ const Player = (props) => {
                 <video controls muted autoPlay ref={videoRef} style={{ width: '70%', height: '60%' }}>
                     <source src={`http://localhost:4000/video/${videoId}`} type="video/mp4"></source>
                 </video>
-                <button onClick={handleForward}>Forward 10 seconds</button>
-
             </header>
         </div>
     );
