5a5e13f785d131c4bcdf4fb016c181c27775c7d4
adarsh <adarsh@growpital.com>
2023-06-27 23:37:16 +0530

    added sync and seek feature

--- src/localPlayer.js
+++ src/localPlayer.js
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import {io} from 'socket.io-client'
 function LocalPlayer() {
+  const [timeChanged,setTimeChanged] = useState(0)
   const videoRef = useRef(null);
-  // const socket  = io('http://localhost:4000/')
   const socket  = io('https://video-app-g2dr.onrender.com')
+  // const socket  = io('http://localhost:4000/')
 
   useEffect(() => {
     // Socket event listeners
@@ -12,26 +13,42 @@ function LocalPlayer() {
     });
 
     socket.on('videoForward',(data)=>{
-      console.log('time changed'+data);
+      console.log('socket videoForward event '+data);
+      setTimeChanged(videoRef.current.currentTime)
       videoRef.current.currentTime = data
     })
-    
+
+    socket.on('videoTimeChanged',(data)=>{
+      console.log('socket videoTimeChanged event '+ data);
+      setTimeChanged(videoRef.current.currentTime)
+      videoRef.current.currentTime = data
+
+    })
     socket.on('disconnect', () => {
       console.log('Disconnected from server');
     });
 
     // Clean up the socket connection
-    return () => {
-      socket.disconnect();
-    };
+    // return () => {
+    //   socket.disconnect();
+    // };
   }, []);
 
 
-  const handleForward = () => {
+  
+  const syncVideo = () => {
     if (videoRef.current) {
-      videoRef.current.currentTime += 10; // Forward by 10 seconds
-      console.log('video forward to ' + videoRef.current.currentTime);
+      console.log('syncVideo ' + videoRef.current.currentTime);
+      setTimeChanged(videoRef.current.currentTime)
       socket.emit('videoForward',videoRef.current.currentTime)
+    }
+  };
+
+  const handleSeeked = () => {
+    if(videoRef.current.currentTime !== timeChanged){
+      console.log('User clicked on the video timeline ' + videoRef.current.currentTime);
+      setTimeChanged(videoRef.current.currentTime)
+      socket.emit('videoTimeChanged',videoRef.current.currentTime)
     }
   };
 
@@ -47,8 +64,10 @@ function LocalPlayer() {
   return (
     <div>
       <input type="file" accept="video/*" onChange={handleFileChange} />
-      <video ref={videoRef} controls  style={{height:'60%',width:'80%'}}/>
-      <button onClick={handleForward}>forward</button>
+      <video ref={videoRef} controls 
+       onSeeked={handleSeeked} 
+       style={{height:'60%',width:'80%'}}/>
+      <button onClick={syncVideo}>sync video</button>
 
     </div>
   );
