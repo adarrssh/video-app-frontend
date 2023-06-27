89ede892a3cd61e64f10a0db9ad7ac98848e0cf3
adarsh <adarsh@growpital.com>
2023-06-27 11:20:24 +0530

    added a page to store video

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Home from './Home';
 import Player from './Player';
 import UploadVideo from './Upload';
+import UploadVideotoStorage from './uploadtoStorage';
 
 function App() {
     return (
@@ -21,16 +22,20 @@ function App() {
                         <Link to="/upload">Upload Video</Link>
                     </li>
                     <li>
-                        <Link to="/player/64984f12d320e100654ae778" target='_blank'>Video 1</Link>
+                        <Link to="/upload-to-localstorage">Upload Video</Link>
                     </li>
                     <li>
-                        <Link to="/player/6499bb067ee1e113c4ec3261" target='_blank'>Video 2</Link>
+                        <Link to="/player/panchayat.mp4" target='_blank'>Video 1</Link>
+                    </li>
+                    <li>
+                        <Link to="/player/panchayat.mp4" target='_blank'>Video 2</Link>
                     </li>
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route exact path="/upload" element={<UploadVideo />} />
+                <Route exact path="/upload-to-localstorage" element={<UploadVideotoStorage />} />
                 <Route path="/player/:id" element={<Player />} />
             </Routes>
         </Router>
--- src/Player.js
+++ src/Player.js
@@ -5,18 +5,18 @@ const Player = () => {
   const { id:videoId } = useParams();
   const videoRef = useRef(null);
 
-  const socket = io('https://video-app-g2dr.onrender.com'); // Replace with your server URL
   useEffect(() => {
+    const socket = io('http://localhost:4000'); // Replace with your server URL
 
     // Socket event listeners
     socket.on('connect', () => {
       console.log('Connected to server');
     });
 
-    // socket.on('videoTimeChanged',(data)=>{
-    //   console.log(data);
-    //   videoRef.current.currentTime = data
-    // })
+    socket.on('videoTimeChanged',(data)=>{
+      console.log('time changed'+data);
+      videoRef.current.currentTime = data
+    })
     
     socket.on('disconnect', () => {
       console.log('Disconnected from server');
@@ -31,22 +31,24 @@ const Player = () => {
   const handleForward = () => {
     if (videoRef.current) {
       videoRef.current.currentTime += 10; // Forward by 10 seconds
+      console.log('video forward to ' + videoRef.current.currentTime);
+      // socket.emit('videoForward',videoRef.current.currentTime)
     }
   };
 
-  const handleSeeked = () => {
-    console.log('User clicked on the video timeline '+videoRef.current.currentTime)
-    // socket.emit('videoTimeChanged',videoRef.current.currentTime)
-  };
+  // const handleSeeked = () => {
+  //   console.log('User clicked on the video timeline '+videoRef.current.currentTime)
+  //   socket.emit('videoTimeChanged',videoRef.current.currentTime)
+  // };
 
 
     return (
         <div className="player"  style={{ width: '70%', height: '60%' }}>
             <header className="App-header">
                 <video controls muted autoPlay ref={videoRef} 
-                onSeeked={handleSeeked}
+                // onSeeked={handleSeeked}
                  style={{ width: '70%', height: '60%' }}>
-                    <source src={`https://video-app-g2dr.onrender.com/video/${videoId}`} type="video/mp4"></source>
+                    <source src={`http://localhost:4000/stream/${videoId}`} type="video/mp4"></source>
                 </video>
                 <button onClick={handleForward}>forward</button>
             </header>
