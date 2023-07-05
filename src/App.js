de3480861e76ba48384636301a23554da2a64fe6
adarsh <adarsh@growpital.com>
2023-07-05 19:11:11 +0530

    feat: added rooms

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import LocalPlayer from './localPlayer';
+import UserB from './JoinRoom';
 
 function App() {
     return (
@@ -19,11 +20,15 @@ function App() {
                     <li>
                         <Link to="/play">go to player</Link>
                     </li>
+                    <li>
+                        <Link to="/join">join room</Link>
+                    </li> 
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route exact path="/play" element={<LocalPlayer />} />
+                <Route exact path="/join" element={<UserB />} />
             </Routes>
         </Router>
     );
--- src/localPlayer.js
+++ src/localPlayer.js
@@ -1,11 +1,12 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('https://video-app-g2dr.onrender.com');
-// const socket = io('http://localhost:4000/');
+// const socket = io('https://video-app-g2dr.onrender.com');
+const socket = io('http://localhost:4000/');
 
 function LocalPlayer() {
   const videoRef = useRef(null);
+  const [roomId, setRoomId] = useState('');
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -19,45 +20,53 @@ function LocalPlayer() {
       console.log('Connected to server');
     };
 
-    const handlePlayBroadcast = () => {
-      console.log('playBroadcast');
-      if (videoRef.current && videoRef.current.paused) {
-        videoRef.current.play();
-      }
-    };
+    socket.on('roomCreated', (roomId) => {
+      setRoomId(roomId);
+      console.log('created room id',roomId);
+    });
 
-    const handlePauseBroadcast = () => {
-      console.log('pauseBroadcast');
-      if (videoRef.current && !videoRef.current.paused) {
-        videoRef.current.pause();
-      }
-    };
+    socket.on('userJoined',(id)=>{
+      console.log(id, ' joined the room');
+    })
+    // const handlePlayBroadcast = () => {
+    //   console.log('playBroadcast');
+    //   if (videoRef.current && videoRef.current.paused) {
+    //     videoRef.current.play();
+    //   }
+    // };
 
-    const handleBroadcastTime = (time) => {
-      const difference = Math.abs(videoRef.current.currentTime - time);
-      console.log(difference);
-      if (videoRef.current && difference > 1) {
-        console.log('broadcastTime', time);
-        videoRef.current.currentTime = time;
-      }
-    };
+    // const handlePauseBroadcast = () => {
+    //   console.log('pauseBroadcast');
+    //   if (videoRef.current && !videoRef.current.paused) {
+    //     videoRef.current.pause();
+    //   }
+    // };
+
+    // const handleBroadcastTime = (time) => {
+    //   const difference = Math.abs(videoRef.current.currentTime - time);
+    //   console.log(difference);
+    //   if (videoRef.current && difference > 1) {
+    //     console.log('broadcastTime', time);
+    //     videoRef.current.currentTime = time;
+    //   }
+    // };
 
     const handleDisconnect = () => {
       console.log('Disconnected from server');
     };
 
     socket.on('connect', handleConnect);
-    socket.on('playBroadcast', handlePlayBroadcast);
-    socket.on('pauseBroadcast', handlePauseBroadcast);
-    socket.on('broadcastTime', handleBroadcastTime);
+    // socket.on('playBroadcast', handlePlayBroadcast);
+    // socket.on('pauseBroadcast', handlePauseBroadcast);
+    // socket.on('broadcastTime', handleBroadcastTime);
     socket.on('disconnect', handleDisconnect);
 
     // Clean up the socket connection
     return () => {
       socket.off('connect', handleConnect);
-      socket.off('playBroadcast', handlePlayBroadcast);
-      socket.off('pauseBroadcast', handlePauseBroadcast);
-      socket.off('broadcastTime', handleBroadcastTime);
+      // socket.off('playBroadcast', handlePlayBroadcast);
+      // socket.off('pauseBroadcast', handlePauseBroadcast);
+      // socket.off('broadcastTime', handleBroadcastTime);
       socket.off('disconnect', handleDisconnect);
       socket.disconnect();
     };
@@ -66,30 +75,28 @@ function LocalPlayer() {
   const handleSeeked = () => {
     if (videoRef.current) {
       console.log('handleSeeked', videoRef.current.currentTime);
-      socket.emit('timeChanged', videoRef.current.currentTime);
+      socket.emit('timeChanged', {roomId,time:videoRef.current.currentTime});
     }
   };
 
   const handlePlay = () => {
     console.log('handlePlay');
-    socket.emit('play');
+    socket.emit('play',roomId);
   };
 
   const handlePause = () => {
     console.log('handlePause');
-    socket.emit('pause');
+    socket.emit('pause',roomId);
   };
 
-  const handleMouseEnter = () => {
-    if (videoRef.current) {
-      videoRef.current.pause();
-    }
+  const handleCreateRoom = () => {
+    socket.emit('createRoom');
   };
 
   return (
     <>
       <input type="file" accept="video/*" onChange={handleFileChange} />
-      <div onMouseEnter={handleMouseEnter}>
+      <div style={{background:'grey'}}>
         <video
           ref={videoRef}
           controls
@@ -99,6 +106,7 @@ function LocalPlayer() {
           onPause={handlePause}
         />
       </div>
+      <button onClick={handleCreateRoom}>Create Room</button>
     </>
   );
 }
